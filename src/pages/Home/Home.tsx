5e6d7ddd75d1369f4fac0c3cca8ab88a5f5b76eb
zerontec <zerontecnologia1@gmail.com>
2023-02-18 01:37:16 -0400

    Update Home Component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React,{useEffect, useState} from 'react';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { people } from '@/data';
+import { Person } from '@/models';
+import { Checkbox } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite } from '@/redux/states/favorites';
+import { addPeople } from '@/redux/states';
+import { People } from '@mui/icons-material';
+import store, { AppStore } from '@/redux/store';
+import { PeopleTable } from './components';
+
+
+
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-	return <div>
-<DataGrid 
-			disableColumnSelector
-			disableSelectionOnClick
-			autoHeight columns={[]} rows={[]}
-			
-/>
+	const dispatch = useDispatch();
+	useEffect(() =>{
+
+		dispatch(addPeople(People))
+
+	}, [])
+	
+	return <PeopleTable/>
 
 
 
 
 
-	</div>;
+
+	
 };
 
 export default Home;
