51466d88d286e051d3922aadb7dd3f1e3213824c
zerontec <zerontecnologia1@gmail.com>
2023-02-18 01:33:59 -0400

    Update App.tsx with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 
 
+import { Provider } from 'react-redux'
 import './App.css'
 import { Navbar } from './components'
+import { Home } from './pages'
+import { LayoutContainer } from './style-components'
+import store from './redux/store'
 
 function App() {
  
 
   return (
-  <>
+  <Provider store={store}> 
+  
 <Navbar/>
 
+<LayoutContainer>
 
-</>
+<Home/>
+
+</LayoutContainer>
+
+
+</Provider> 
+
+
   )
 }
 
