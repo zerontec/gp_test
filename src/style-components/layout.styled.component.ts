132fc2eb10f06cb9b5ec75be77a33176f3d9e917
zerontec <zerontecnologia1@gmail.com>
2023-02-18 10:12:55 -0400

    install style components

---
+++ src/style-components/layout.styled.component.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+
+
+
+width:80vw;
+
+
+@media only screen and (min-width:1176px){
+
+padding:12px;
+
+}
+    
+
+`
